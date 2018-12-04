6e49d5c669d38f069eb86a578e2f7a7a8bc7d51b
EddieSwift <37484195+EddieSwift@users.noreply.github.com>
2018-12-04 19:35:39 +0200

    Initial Commit

---
+++ 23GesturesHW/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  23GesturesHW
+//
+//  Created by Eduard Galchenko on 12/4/18.
+//  Copyright © 2018 Eduard Galchenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
